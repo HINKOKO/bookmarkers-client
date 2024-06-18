0b6efd1de35c040f0d85c1c5e26905b7821be99b
HINKOKO <piotr62@live.fr>
2024-06-18 11:22:58 +0200

    ability to modify avatar

--- src/components/Dashboard/EditProfile.jsx
+++ src/components/Dashboard/EditProfile.jsx
@@ -1,20 +1,65 @@
 import { useState, useRef } from 'react';
 import { useAuth } from '../../store/AuthContext';
 
+const backend = `http://localhost:8080`;
+
 const EditProfile = () => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
   const [newPass, setNewPass] = useState('');
-  const [avatar, setAvatar] = useState('');
+  const [newPassConfirm, setNewPassConfirm] = useState('');
+  const [avatar, setAvatar] = useState(null);
+  const [preview, setPreview] = useState(null);
 
   const { user } = useAuth();
   const hiddenFileInput = useRef(null);
 
-  console.log(user);
+  // console.log(user);
+
+  const handleFileChange = e => {
+    const file = e.target.files[0];
+    setAvatar(file);
+    setPreview(URL.createObjectURL(file));
+  };
+
+  // function to programaticaly click the hidden file input when the button is clicked
+  const handleButtonClick = () => {
+    hiddenFileInput.current.click();
+  };
+
+  const handleUpload = async () => {
+    if (avatar) {
+      const formData = new FormData();
+      formData.append('avatar', avatar);
+
+      console.log(`${backend}/dashboard/upload-avatar`);
+
+      try {
+        const response = await fetch(`${backend}/dashboard/upload-avatar`, {
+          method: 'POST',
+          body: formData,
+          credentials: 'include',
+        });
+        if (!response.ok) {
+          throw new Error('upload failed');
+        }
+        const data = await response.json();
+        console.log('upload successfull', data);
+      } catch (error) {
+        console.error('error uploading your avatar', error);
+      }
+    }
+  };
 
   return (
     <div className="container flex w-11/12 bg-orange-200 mx-auto">
-      <form className="mx-auto w-4/5 mt-4 space-y-8">
+      <form
+        className="mx-auto w-4/5 mt-4 space-y-8"
+        onSubmit={e => {
+          e.preventDefault();
+          handleUpload();
+        }}
+      >
         <div className="flex flex-row justify-center items-center space-between gap-8">
           <label
             htmlFor="current_email"
@@ -28,14 +73,18 @@ const EditProfile = () => {
           >
             <div className="bg-slate-300 p-8 rounded-tl-lg rounded-bl-lg">
               <img
-                src={user.avatar_url}
+                src={preview || user.avatar_url}
                 alt="current_avatar"
                 className="rounded-md"
                 width={46}
                 height={46}
               />
             </div>
-            <button className="font-thin text-lg font-kanit bg-blue-400">
+            <button
+              type="button"
+              className="font-thin text-lg font-kanit bg-blue-400"
+              onClick={handleButtonClick}
+            >
               load a new avatar
             </button>
             <input
@@ -43,6 +92,8 @@ const EditProfile = () => {
               type="file"
               ref={hiddenFileInput}
               style={{ display: 'none' }}
+              accept="image/*"
+              onChange={handleFileChange}
             />
 
             {/* <button style="display:block;width:120px; height:30px;" onclick="document.getElementById('getFile').click()">Your text here</button>
@@ -60,8 +111,8 @@ const EditProfile = () => {
             type="text"
             className="border text-sm rounded-lg block w-full p-2.5"
             maxLength={2048}
-            placeholder={user.email ? user.email : 'Set your email'}
-            required
+            placeholder={user.email || email || 'Set your email'}
+            onChange={e => setEmail(e.target.value)}
           />
         </div>
         <div>
@@ -76,7 +127,7 @@ const EditProfile = () => {
             className="border text-sm rounded-lg block w-full p-2.5"
             maxLength={2048}
             placeholder="Please enter current password"
-            required
+            onChange={e => setPass(e.target.value)}
           />
         </div>
         <div>
@@ -91,7 +142,7 @@ const EditProfile = () => {
             className="border text-sm rounded-lg block w-full p-2.5"
             maxLength={2048}
             placeholder="New password"
-            required
+            onChange={e => setNewPass(e.target.value)}
           />
         </div>
         <div>
@@ -106,10 +157,13 @@ const EditProfile = () => {
             className="border text-sm rounded-lg block w-full p-2.5"
             maxLength={2048}
             placeholder="Confirm new password"
-            required
+            onChange={e => setNewPassConfirm(e.target.value)}
           />
         </div>
-        <button className="mt-6 px-4 py-4 border w-2/3 rounded-xl bg-amber-700 text-lg font-medium font-kanit">
+        <button
+          type="submit"
+          className="mt-6 px-4 py-4 border w-2/3 rounded-xl bg-amber-700 text-lg font-medium font-kanit"
+        >
           Save changes
         </button>
       </form>
